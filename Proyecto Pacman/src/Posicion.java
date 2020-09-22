06881aebb4ac4f5aa3f8c2d188dcca129f1790b5
sergiocarp10 <bs.sergiocalderon@gmail.com>
2020-09-22 23:08:50 +0100

    first commit

---
+++ Proyecto Pacman/src/Posicion.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+/**
+ * Representa una ubicación en el plano dada por un par de valores (x,y)
+ * @author Calderón Sergio, Juan Martín Ercoli
+ * @version 1
+ */
+public class Posicion {
+	int x, y;
+
+	/**
+	 * Constructor que permite inicializar variables
+	 * @param x el valor de la abscisa (eje X)
+	 * @param y el valor de la ordenada (eje Y)
+	 */
+	public Posicion(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * Constructor por defecto, sin parámetros
+	 */
+	public Posicion() {
+		
+	}
+	
+	
+}
