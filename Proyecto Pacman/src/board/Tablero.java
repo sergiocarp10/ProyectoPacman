fd3d260ef3627666f6ff2f56f4b30c75c145a452
sergiocarp10 <bs.sergiocalderon@gmail.com>
2020-09-27 10:13:11 +0100

    agrego tablero en board

---
+++ Proyecto Pacman/src/board/Tablero.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package board;
+
+/**
+ * 
+ * @author Calderón Sergio, Ercoli Juan Martín
+ * @version 1
+ */
+public class Tablero {
+	private static final byte MAX_SIZE = 32;
+	private int cantPuntosInicio, cantPuntosRestantes;
+	private TipoCelda[][] matriz;
+	
+	/**
+	 * Constructor sin parámetros, inicializa un tablero vacío de dimensiones por defecto
+	 */
+	public Tablero() {
+		this.matriz = new TipoCelda[MAX_SIZE][MAX_SIZE];
+	}
+	
+	/**
+	 * Carga el tablero con los tipos de celda correspondientes
+	 */
+	public void crearPorDefecto() {
+		// Implementación
+	}
+	
+	/**
+	 * 
+	 * @param x el valor de abscisas a consultar
+	 * @param y el valor de ordenada a consultar
+	 * @return si la celda en la coordenada (x,y) es transitable
+	 */
+	public boolean esTransitable(int x, int y) {
+		// Implementación
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param x el valor de abscisas a consultar
+	 * @param y el valor de ordenada a consultar
+	 * @return si la celda en la coordenada (x,y) es un túnel
+	 */
+	public boolean esTunel(int x, int y) {
+		return this.matriz[x][y] == TipoCelda.TUNEL;
+	}
+	
+	/**
+	 * 
+	 * @return Un arreglo con los valores (x,y) correspondientes a la casa.
+	 */
+	public int[] getEspacioCasa() {
+		// Implementación
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param x el valor de abscisas a consultar
+	 * @param y el valor de ordenada a consultar
+	 * @return si la celda aún contiene un punto
+	 */
+	public boolean tieneComida(int x, int y) {
+		return (this.matriz[x][y] == TipoCelda.PUNTO);
+	}
+	
+	/**
+	 * 
+	 * @param x el valor de abscisas a consultar
+	 * @param y el valor de ordenada a consultar
+	 * @return si la celda contiene una de las 4 frutas
+	 */
+	public boolean tieneFruta(int x, int y) {
+		return (this.matriz[x][y] == TipoCelda.FRUTA);
+	}
+	
+	/**
+	 * Establece una celda como libre
+	 * @param x el valor de abscisas a cambiar
+	 * @param y el valor de ordenada a cambiar
+	 */
+	public void quitarPunto(int x, int y) {
+		this.matriz[x][y] = TipoCelda.LIBRE;
+	}
+}
