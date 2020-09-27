c6b9aad4960fc3dd26942d9c84a7c359836f8a25
sergiocarp10 <bs.sergiocalderon@gmail.com>
2020-09-27 09:34:57 +0100

    agrego paquete board para separarlo de la implementacion de personajes

--- Proyecto Pacman/src/Direccion.java
+++ Proyecto Pacman/src/Direccion.java
@@ -6,7 +6,7 @@
  * Enumerativo con las únicas 4 direcciones permitidas en el juego: 
  * UP (arriba), DOWN (abajo), LEFT (izquierda), RIGHT (derecha).
  * @author Calderón Sergio, Ercoli Juan Martín
- *
+ * @version 1
  */
 public enum Direccion {
 	UP, DOWN, LEFT, RIGHT
