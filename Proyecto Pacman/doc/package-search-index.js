2ec970a5602a2887f25b298e6ae67f6bdc800463
sergiocarp10 <bs.sergiocalderon@gmail.com>
2020-09-28 02:50:54 +0100

    javadoc 2

--- Proyecto Pacman/doc/package-search-index.js
+++ Proyecto Pacman/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"personajes"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"board"},{"l":"personajes"},{"l":"utils"}]
